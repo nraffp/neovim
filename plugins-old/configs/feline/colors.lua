7ea61cbfa71fa6a0440ea833d10d5827b78789bf
Nick Raffaele <raffaele.nickp@gmail.com>
2025-07-16 08:17:19 -0700

    remove plugins old

--- plugins-old/configs/feline/colors.lua
+++
@@ -1,4 +0,0 @@
-local feline = require("feline")
-local p = require("theme.palette")
-
-return vim.tbl_extend("force", p, { bg = p.dark_3, bg_light = p.dark_4 })
--- plugins-old/configs/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-require("toggleterm").setup({
-    open_mapping = [[<c-\>]],
-    direction = "float",
-    float_opts = {
-        border = "single",
-    },
-    shade_terminals = false,
-})
